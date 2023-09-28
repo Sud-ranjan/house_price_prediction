a881e52f3972efab51f0a16349192e095e7cd5bf
Sud-ranjan <sud.ranjan999@gmail.com>
2023-09-28 16:24:29 +0530

    addedd form and a route for the same to collect data from homepage and show predicted output

--- app.py
+++ app.py
@@ -20,9 +20,19 @@ def predict_api():
     #  print(data)
     transformed_data = normalizer.transform(np.array(list(data.values())).reshape(1,-1))
     output = model.predict(transformed_data)
-    print(output[0])
+    # print(output[0])
 
     return jsonify(output[0])
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    transformed_data = normalizer.transform(np.array(data).reshape(1,-1))
+    # print(transformed_data)
+    output = model.predict(transformed_data)[0]
+    return render_template("home.html", prediction_text = "The predicted house price is ${}".format(output*1000))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
